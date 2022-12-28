38b619d2ea17c255d8d3e5c7312f379c0f5db4f9
GuilhermeTome <guigo.reis90@gmail.com>
2022-12-28 02:39:58 -0300

    feat: add more 1 default user

--- migrations/003_insert_base_users.sql
+++ migrations/003_insert_base_users.sql
@@ -1,2 +1,3 @@
 -- $2y$10$mbzXhabrtahAfjidLJjJse8TI6IGhM/HVpDnMJQwluuhMAyqYKj0m => password
-INSERT INTO `users` (`name`, `email`, `password`) VALUES ('RH Fastview', 'rhfast@fastview.com.br', '$2y$10$mbzXhabrtahAfjidLJjJse8TI6IGhM/HVpDnMJQwluuhMAyqYKj0m')
+INSERT INTO `users` (`name`, `email`, `password`) VALUES ('RH Fastview', 'rhfast@fastview.com.br', '$2y$10$mbzXhabrtahAfjidLJjJse8TI6IGhM/HVpDnMJQwluuhMAyqYKj0m'),
+('Eduardo Teixeira', 'eduardoosteixeira@gmail.com', '$2y$10$mbzXhabrtahAfjidLJjJse8TI6IGhM/HVpDnMJQwluuhMAyqYKj0m')
